7118626f3fa847417d98819d78034e8a15d227c5
Renotrader31 <lagiog31@gmail.com>
2025-09-19 11:57:17 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
- 1	/** @type {import('next').NextConfig} */
-     2	const nextConfig = {
-     3	  reactStrictMode: true,
-     4	  swcMinify: true,
-     5	  env: {
-     6	    POLYGON_API_KEY: process.env.POLYGON_API_KEY,
-     7	  },
-     8	  async rewrites() {
-     9	    return [
-    10	      {
-    11	        source: '/api/:path*',
-    12	        destination: process.env.NODE_ENV === 'development' 
-    13	          ? 'http://localhost:8000/:path*'
-    14	          : '/api/:path*',
-    15	      },
-    16	    ];
-    17	  },
-    18	  images: {
-    19	    remotePatterns: [
-    20	      {
-    21	        protocol: 'https',
-    22	        hostname: 'polygon.io',
-    23	      },
-    24	    ],
-    25	  },
-    26	  experimental: {
-    27	    serverComponentsExternalPackages: ['axios'],
-    28	  },
-    29	}
-    30	
-    31	module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    POLYGON_API_KEY: process.env.POLYGON_API_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:8000/:path*'
+          : '/api/:path*',
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'polygon.io',
+      },
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['axios'],
+  },
+}
+
+module.exports = nextConfig
